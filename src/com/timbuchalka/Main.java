793738b3f68f41007b7476ecf8aee6b142f47625
Vanita Singh <vanitasinghiitd@gmail.com>
2021-01-23 23:27:47 +0530

    Test Commit and git ignore

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -14,6 +14,7 @@ import java.util.Map;
  */
 public class Main {
     private static StockList stockList = new StockList();
+    //Test Commit
 
     public static void main(String[] args) {
 	    StockItem temp = new StockItem("bread", 0.86, 100);
